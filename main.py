c3d1c6d74603ada18d16217181f4f21cc727bf38
Blesson K Tomy <116932677+Blesson-Tomy@users.noreply.github.com>
2024-02-25 23:08:27 +0530

    added file checking

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-import os, sys
+import os
+import sys
 import cv2
 import numpy as np
 import face_recognition
 import math
 
+
 def face_confidence(face_distance, face_match_threshold=0.6):
     range = (1.0 - face_match_threshold)
-    linear_val = (1.0 - face_distance) / (range*2.0)
+    linear_val = (1.0 - face_distance) / (range * 2.0)
 
     if face_distance > face_match_threshold:
         return str(round(linear_val * 100, 2)) + '%'
     else:
-        value =(linear_val +((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2,0.2))) * 100
+        value = (linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))) * 100
         return str(round(value, 2)) + '%'
+
 
 class FaceRecognition:
     face_locations = []
@@ -27,11 +30,12 @@ class FaceRecognition:
 
     def encode_faces(self):
         for image in os.listdir('faces'):
-            face_image = face_recognition.load_image_file(f'faces/{image}')
-            face_encoding = face_recognition.face_encodings(face_image)[0]
+            if image.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
+                face_image = face_recognition.load_image_file(f'faces/{image}')
+                face_encoding = face_recognition.face_encodings(face_image)[0]
 
-            self.known_face_encodings.append(face_encoding)
-            self.known_face_names.append(image)
+                self.known_face_encodings.append(face_encoding)
+                self.known_face_names.append(image)
 
         print(self.known_face_names)
 
@@ -48,7 +52,7 @@ class FaceRecognition:
                 small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
                 rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
 
-                #find all faces
+                # find all faces
                 self.face_locations = face_recognition.face_locations(rgb_small_frame)
                 self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
 
@@ -68,16 +72,16 @@ class FaceRecognition:
                     self.face_names.append(f'{name} ({confidence})')
             self.process_current_frame = not self.process_current_frame
 
-            #Display Annotations
-            for(top, right, bottom, left), name in zip(self.face_locations, self.face_names):
+            # Display Annotations
+            for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
                 top *= 4
                 right *= 4
                 bottom *= 4
                 left *= 4
 
-                cv2.rectangle(frame, (left, top), (right, bottom), (0,0,225), 2)
-                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0,0,225), -1)
-                cv2.putText(frame, name, (left + 6, bottom -6),cv2.FONT_HERSHEY_DUPLEX,0.85,(255,255,255),1)
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 225), 2)
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 225), -1)
+                cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.85, (255, 255, 255), 1)
 
             cv2.imshow('Face Recognition', frame)
 
@@ -91,5 +95,3 @@ class FaceRecognition:
 if __name__ == '__main__':
     fr = FaceRecognition()
     fr.run_recognition()
-
-
